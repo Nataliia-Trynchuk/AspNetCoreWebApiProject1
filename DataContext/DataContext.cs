420b4dd3b9087583c33d5636b54ea38ec788380c
Dmytro Strembetskyi <Dmytro.Strembetskyi@flir.com>
2024-09-19 14:49:51 +0300

    All

---
+++ DataContext/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using AspNetCoreWebApiProject1.Models;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbContextOptions;
+
+namespace AspNetCoreWebApiProject1.DataContext
+{
+    public class DataContext: DbContext
+    {
+        // constructor
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        // DbSet<Movies> property representing the entity set
+        // entity set generally aligns with a database table,
+        // and an entity corresponds to an individual row within that table
+        public DbSet<Movies> Movies { get; set; } = null!;
+    }
+}
